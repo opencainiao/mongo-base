c590c3366898ad1152dce6882b6d0281e23da2ba
opencainiao <25662996@qq.com>
2015-10-09 12:03:53 +0800

    增加模糊查询用的Like 增加测试用例

---
+++ src/main/java/com/mou/mongodb/base/util/RegexPatternUtil.java
@@ -0,0 +1,100 @@
+package com.mou.mongodb.base.util;
+
+import java.util.regex.Pattern;
+
+import org.mou.common.StringUtil;
+
+public class RegexPatternUtil {
+
+	/***
+	 * 获取用于like的正则表达式
+	 * 
+	 * @param strIn
+	 * @return
+	 */
+	public static Pattern getLikePattern(String strIn) {
+
+		String use = null;
+
+		if (strIn == null || strIn.trim().length() == 0) {
+			use = "";
+		} else {
+			use = strIn.trim();
+		}
+
+		return Pattern.compile(".*" + use + ".*", Pattern.CASE_INSENSITIVE);
+	}
+
+	/***
+	 * 获取用于左like的正则表达式
+	 * 
+	 * 
+	 * @param strIn
+	 * @return
+	 */
+	public static Pattern getLeftLikePattern(String strIn) {
+
+		String use = null;
+
+		if (strIn == null || strIn.trim().length() == 0) {
+			use = "";
+		} else {
+			use = strIn.trim();
+		}
+
+		return Pattern.compile("^" + use + ".*$", Pattern.CASE_INSENSITIVE);
+	}
+
+	/***
+	 * 获取用于右like的正则表达式
+	 * 
+	 * 
+	 * @param strIn
+	 * @return
+	 */
+	public static Pattern getRightLikePattern(String strIn) {
+
+		String use = null;
+
+		if (strIn == null || strIn.trim().length() == 0) {
+			use = "";
+		} else {
+			use = strIn.trim();
+		}
+
+		return Pattern.compile("^" + use + ".*name$", Pattern.CASE_INSENSITIVE);
+	}
+
+	/***
+	 * 获取用于等于的正则表达式，（忽略大小写）
+	 * 
+	 * @param strIn
+	 * @return
+	 */
+	public static Pattern getEqualPattern(String strIn) {
+
+		String use = null;
+
+		if (strIn == null || strIn.trim().length() == 0) {
+			use = "";
+		} else {
+			use = strIn.trim();
+		}
+
+		return Pattern.compile(use, Pattern.CASE_INSENSITIVE);
+	}
+
+	/****
+	 * 判断一个字符串是否只包含26个字母（大小写）
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isOnlyZimu(String str) {
+
+		if (StringUtil.isEmpty(str)) {
+			return false;
+		}
+		return str.matches("^[A-Za-z]+$");
+	}
+}
--- src/test/java/com/mou/mongodb/base/springmongo/FindBatchUtilOriTest.java
+++ src/test/java/com/mou/mongodb/base/springmongo/FindBatchUtilOriTest.java
@@ -82,9 +82,9 @@ public class FindBatchUtilOriTest {
 
 		System.out.println("总条数:" + addresses.size());
 		for (Address address : addresses) {
-			 System.out.println(JsonUtil.toJsonStr(address));
+			System.out.println(JsonUtil.toJsonStr(address));
 		}
-		
+
 		// 3.复合实体对象-类与表匹配
 		orderBy = new BasicDBObject();
 		orderBy.put("sex", 1);
@@ -102,52 +102,321 @@ public class FindBatchUtilOriTest {
 
 	@Test
 	public void testFindAllDBObjectDBObjectDBObjectClassOfT() {
-		fail("Not yet implemented");
+
+		// 1.单实体对象
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("type_value", 1);
+		orderBy.put("_id", -1);
+		List<DBObject> addresses = FindBatchUtilOri.findAllDBObject(orderBy, Address.class);
+
+		for (DBObject address : addresses) {
+			System.out.print(address.get("_id").toString() + "----");
+			System.out.println(address);
+		}
+
+		// 2.复合实体对象
+		orderBy = new BasicDBObject();
+		orderBy.put("sex", 1);
+
+		List<DBObject> clients = FindBatchUtilOri.findAllDBObject(orderBy, Client.class);
+
+		for (DBObject client : clients) {
+			System.out.print(client.get("_id").toString() + "----");
+			System.out.println(client.get("address_info"));
+			System.out.println(client.get("phone_info"));
+			System.out.println(client);
+		}
 	}
 
 	@Test
 	public void testFindAllDBObjectDBObjectDBObjectClassOfTString() {
-		fail("Not yet implemented");
+		// 1.单实体对象
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("type_value", 1);
+		orderBy.put("_id", -1);
+		List<DBObject> addresses = FindBatchUtilOri.findAllDBObject(orderBy, Address.class, "address");
+
+		for (DBObject address : addresses) {
+			System.out.print(address.get("_id").toString() + "----");
+			System.out.println(address);
+		}
+		System.out.println("------------------------------------------------");
+
+		// 2.复合实体对象-类与表不匹配1
+		orderBy = new BasicDBObject();
+		orderBy.put("sex", 1);
+
+		List<DBObject> clients = FindBatchUtilOri.findAllDBObject(orderBy, Client.class, "aa");
+
+		System.out.println("总条数:" + clients.size());
+		for (DBObject client : clients) {
+			System.out.print(client.get("_id").toString() + "----");
+			System.out.println(client.get("address_info"));
+			System.out.println(client.get("phone_info"));
+			System.out.println(client);
+		}
+		System.out.println("------------------------------------------------");
+
+		// 2.复合实体对象-类与表不匹配2
+		orderBy = new BasicDBObject();
+		orderBy.put("sex", 1);
+
+		addresses = FindBatchUtilOri.findAllDBObject(orderBy, Address.class, "client");
+
+		System.out.println("总条数:" + addresses.size());
+		for (DBObject address : addresses) {
+			System.out.println(address);
+		}
+		System.out.println("------------------------------------------------");
+		// 3.复合实体对象-类与表匹配
+		orderBy = new BasicDBObject();
+		orderBy.put("sex", 1);
+
+		clients = FindBatchUtilOri.findAllDBObject(orderBy, Client.class, "client");
+
+		System.out.println("总条数:" + clients.size());
+		for (DBObject client : clients) {
+			System.out.print(client.get("_id").toString() + "----");
+			System.out.println(client.get("address_info"));
+			System.out.println(client.get("phone_info"));
+			System.out.println(client);
+		}
 	}
 
 	@Test
 	public void testFindBatchDBObjectDBObjectClassOfT() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+		List<DBObject> addresses = FindBatchUtilOri.findBatchDBObject(query, orderBy, Address.class);
+
+		for (DBObject address : addresses) {
+			System.out.print(address.get("_id").toString() + "----" + address.get("province").toString() + "----"
+					+ address.get("city").toString() + "----");
+			System.out.println(address);
+		}
 	}
 
 	@Test
 	public void testFindBatchDBObjectDBObjectClassOfTString() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+		List<DBObject> addresses = FindBatchUtilOri.findBatchDBObject(query, orderBy, "aa");
+
+		System.out.println("条数：" + addresses.size());
+		for (DBObject address : addresses) {
+			System.out.print(address.get("_id").toString() + "----" + address.get("province").toString() + "----"
+					+ address.get("city").toString() + "----");
+			System.out.println(address);
+		}
+
+		System.out.println("--------------------------------");
+
+		addresses = FindBatchUtilOri.findBatchDBObject(query, orderBy, "address");
+
+		System.out.println("条数：" + addresses.size());
+		for (DBObject address : addresses) {
+			System.out.print(address.get("_id").toString() + "----" + address.get("province").toString() + "----"
+					+ address.get("city").toString() + "----");
+			System.out.println(address);
+		}
 	}
 
 	@Test
 	public void testFindBatchDBObjectDBObjectDBObjectClassOfT() {
-		fail("Not yet implemented");
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+		List<Address> addresses = FindBatchUtilOri.findBatch(query, orderBy, Address.class);
+
+		System.out.println("条数：" + addresses.size());
+		for (Address address : addresses) {
+			System.out.print(address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			System.out.println(address);
+		}
 	}
 
 	@Test
 	public void testFindBatchDBObjectDBObjectDBObjectString() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+		List<Address> addresses = FindBatchUtilOri.findBatch(query, orderBy, Address.class, "aa");
+
+		System.out.println("条数：" + addresses.size());
+		for (Address address : addresses) {
+			System.out.print(address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			System.out.println(address);
+		}
+
+		addresses = FindBatchUtilOri.findBatch(query, orderBy, Address.class, "address");
+
+		System.out.println("条数：" + addresses.size());
+		for (Address address : addresses) {
+			System.out.print(address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			System.out.println(address);
+		}
 	}
 
 	@Test
 	public void testFindBatchPartDBObjectDBObjectDBObjectClassOfT() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+
+		DBObject returnFields = new BasicDBObject();
+		returnFields.put("city", 1);
+		returnFields.put("province", 1);
+
+		List<DBObject> addresses = FindBatchUtilOri.findBatchPartDBObject(query, orderBy, returnFields, Address.class);
+
+		try {
+			System.out.println("条数" + addresses.size());
+			for (DBObject address : addresses) {
+				System.out.println(address);
+				System.out.println(address.get("_id").toString() + "----" + address.get("province").toString() + "----"
+						+ address.get("city").toString() + "----");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testFindBatchPartDBObjectDBObjectDBObjectClassOfTString() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+
+		DBObject returnFields = new BasicDBObject();
+		returnFields.put("city", 1);
+		returnFields.put("province", 1);
+
+		List<DBObject> addresses = FindBatchUtilOri.findBatchPartDBObject(query, orderBy, returnFields, "aa");
+
+		try {
+			System.out.println("条数" + addresses.size());
+			for (DBObject address : addresses) {
+				System.out.println(address);
+				System.out.println(address.get("_id").toString() + "----" + address.get("province").toString() + "----"
+						+ address.get("city").toString() + "----");
+			}
+
+			addresses = FindBatchUtilOri.findBatchPartDBObject(query, orderBy, returnFields, "address");
+
+			try {
+				System.out.println("条数" + addresses.size());
+				for (DBObject address : addresses) {
+					System.out.println(address);
+					System.out.println(address.get("_id").toString() + "----" + address.get("province").toString()
+							+ "----" + address.get("city").toString() + "----");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testFindBatchPartDBObjectDBObjectDBObjectDBObjectClassOfT() {
-		fail("Not yet implemented");
+
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+
+		DBObject returnFields = new BasicDBObject();
+		returnFields.put("city", 1);
+		returnFields.put("province", 1);
+
+		List<Address> addresses = FindBatchUtilOri.findBatchPart(query, orderBy, returnFields, Address.class);
+
+		try {
+			System.out.println("条数" + addresses.size());
+			for (Address address : addresses) {
+				System.out.println(address);
+				System.out.println(
+						address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			}
+
+			System.out.println("-------------------------------------");
+			addresses = FindBatchUtilOri.findBatchPart(query, orderBy, null, Address.class);
+			System.out.println("条数" + addresses.size());
+			for (Address address : addresses) {
+				System.out.println(address);
+				System.out.println(
+						address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testFindBatchPartDBObjectDBObjectDBObjectDBObjectString() {
-		fail("Not yet implemented");
+		DBObject query = new BasicDBObject();
+		query.put("province", 1);
+
+		DBObject orderBy = new BasicDBObject();
+		orderBy.put("city", 1);
+
+		DBObject returnFields = new BasicDBObject();
+		returnFields.put("city", 1);
+		returnFields.put("province", 1);
+
+		List<Address> addresses = FindBatchUtilOri.findBatchPart(query, orderBy, returnFields, Address.class, "aa");
+
+		try {
+			System.out.println("条数" + addresses.size());
+			for (Address address : addresses) {
+				System.out.println(address);
+				System.out.println(
+						address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			}
+
+			addresses = FindBatchUtilOri.findBatchPart(query, orderBy, returnFields, Address.class, "address");
+
+			System.out.println("条数" + addresses.size());
+			for (Address address : addresses) {
+				System.out.println(address);
+				System.out.println(
+						address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			}
+
+			System.out.println("-------------------------------------");
+			addresses = FindBatchUtilOri.findBatchPart(query, orderBy, null, Address.class, "address");
+			System.out.println("条数" + addresses.size());
+			for (Address address : addresses) {
+				System.out.println(address);
+				System.out.println(
+						address.get_id_m() + "----" + address.getProvince() + "----" + address.getCity() + "----");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
